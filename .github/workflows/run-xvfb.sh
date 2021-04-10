1ec723c284849ba4c1efb97d71a249a53a867628
Mark Raynsford <code@io7m.com>
2021-04-10 13:46:05 +0000

    Add xterm

--- .github/workflows/run-xvfb.sh
+++ .github/workflows/run-xvfb.sh
@@ -1,15 +1,12 @@
 #!/bin/bash -ex
 
 sudo apt-get -y install xvfb openbox scrot feh
-
 Xvfb :99 &
-
 export DISPLAY=:99
-
 openbox &
-
+sleep 1
+xterm &
+sleep 1
 feh --bg-tile .github/workflows/wallpaper.png
-
 sleep 3
-
 scrot output.jpg
